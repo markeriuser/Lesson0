88677382aaf5596de5b90d11cd995f9241a02d97
markeriuser <mr.lms29@mail.ru>
2024-08-14 16:20:50 +0300

    Add files via upload

---
+++ lesson0 red.py
@@ -0,0 +1,19 @@
+# 1st program
+print(9 ** 0.5 * 5)
+# 2st program
+print (9.99 > 9.98 and 1000 != 1000.1)
+# 3st program
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print (6 == 8)
+# 3st program verse2
+str1 = (2 * 2 +2)
+str2 = (2 * (2 + 2))
+print (str1 == str2)
+# 4st program
+x = float('123.456')
+y = (x * 10)
+print (int( y % 10 ))
+# 4st program var2
+x = '123.456'
+print(x[x.find('.') + 1])
